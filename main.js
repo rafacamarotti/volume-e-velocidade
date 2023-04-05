51697cb76abc0b6cd59cae02d9e20283d7d7ae3b
rafacamarotti <110203753+rafacamarotti@users.noreply.github.com>
2023-04-05 20:44:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function preload(){
+    song= loadSound("music.mp3");
+}
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+rightWristX = 0;
+rightWristY = 0;
+leftWristX = 0;
+leftWristY= 0;
+function setup(){
+    canvas= createCanvas(600, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses); 
+}
+function modelLoaded(){
+    console.log('PoseNet foi inicializado');
+}
+function gotPoses(results){
+    if(results.lenth >0){
+        console.log(results);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = "+rightWristX+" rightWristY = "+rightWristY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = "+leftWristX+" leftWristY = "+leftWristY);
+    }
+}
