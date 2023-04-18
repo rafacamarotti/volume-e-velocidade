a6125a0660a7c567e16720e109930decf5fce7e0
rafacamarotti <110203753+rafacamarotti@users.noreply.github.com>
2023-04-18 20:06:34 -0300

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function modelLoaded(){
     console.log('PoseNet foi inicializado');
 }
 function gotPoses(results){
-    if(results.lenth >0){
+    if(results.length >0){
         scoreRightWrist = results[0].pose.keypoints[10].score ;
         scoreLeftWrist = results[0].pose.keypoints[9].score ;
         console.log("scoreRightWrist = "+scoreRightWrist+" scoreLeftWrist = "+scoreLeftWrist);
